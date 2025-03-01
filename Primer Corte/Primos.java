ed7d2021c1a63e0c8c1bed965a5b6e9ec1ef22a8
Santiago Orjuela Florez <santiago-orjuelaf@unilibre.edu.co>
2025-03-01 09:52:02 -0500

    Ejercicio 2

---
+++ Primer Corte/Primos.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Primos {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int numero;
+        double raiz;
+        System.out.print("Ingrese un numero: ");
+        numero = leer.nextInt();
+        for (int i = 2; i <= numero; i++) {
+            raiz = Math.sqrt(i);
+            boolean esPrimo = true;
+            for (int j = 2; j <= raiz; j++) {
+                if (i % j == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+            if (esPrimo) {
+                System.out.println(i);
+            }
+        }
+    }
+}
